0d4b78640af5dc644db95653cd8411a516bdc8ea
seanxkao <sean1201@gmail.com>
2017-06-04 00:41:43 +0800

    Updating card skins and background. Add tutorial.

---
+++ Assets/Script/SelectFreeRuleMessagebox.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectFreeRuleMessagebox : Messagebox {
+	void Start () {
+		//2 buttons for 2 rules.
+		buttons[0].onClick.AddListener(() => Manager.manager().loadPuzzleMode(1));
+		buttons[1].onClick.AddListener(() => Manager.manager().loadPuzzleMode(3));
+	}
+
+}
